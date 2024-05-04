22649b5edc49d8a232b72c954c65f86e9164366b
Qollesium <omerorcun_tukenmez@hotmail.com>
2024-05-04 06:02:23 -0700

    Alarm activate deactivate

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\API\User\ActivateAlarmRequest;
+use App\Http\Requests\API\User\AlarmSeenRequest;
 use App\Http\Requests\API\User\AttachCryptoCurrencyRequest;
 use App\Http\Requests\API\User\DetachCryptoCurrencyRequest;
 use App\Models\CryptoCurrency;
+use App\Models\CryptoCurrencyUser;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -78,14 +81,68 @@ class UserController extends Controller
         $user = auth()->user();
         $cryptoCurrency = CryptoCurrency::find($request->crypto_currency_id);
 
-        $user->cryptoCurrencies()->attach($cryptoCurrency, [
-            'analyze_method' => $request->analyze_method,
-            'analyze_alarm' => $request->analyze_alarm,
-            'analyze_alarm_percent' => $request->analyze_alarm_percent,
-            'is_active' => $request->is_active,
-        ]);
+        $existingRecord = CryptoCurrencyUser::where('user_id', $user->id)->where('crypto_currency_id', $cryptoCurrency->id)->first();
+
+        if ($existingRecord) {
+            $existingRecord->update([
+                'analyze_method' => $request->analyze_method,
+                'analyze_alarm' => $request->analyze_alarm,
+                'analyze_alarm_percent' => $request->analyze_alarm_percent,
+                'analyze_alarm_activated_date' => null,
+                'is_analyze_alarm_activated' => false,
+                'is_active' => $request->is_active,
+            ]);
+        } else {
+            // Mevcut kayıt yoksa yeni bir kayıt ekleyelim
+            $user->cryptoCurrencies()->attach($cryptoCurrency, [
+                'analyze_method' => $request->analyze_method,
+                'analyze_alarm' => $request->analyze_alarm,
+                'analyze_alarm_percent' => $request->analyze_alarm_percent,
+                'is_active' => $request->is_active,
+            ]);
+        }
 
         return response()->json(['message' => 'CryptoCurrency attached successfully'], 200);
+    }
+
+    public function alarmSeen(AlarmSeenRequest $request) {
+        $user = auth()->user();
+
+        $cryptoCurrency = CryptoCurrency::find($request->crypto_currency_id);
+
+        $existingRecord = CryptoCurrencyUser::where('user_id', $user->id)->where('crypto_currency_id', $cryptoCurrency->id)->first();
+
+        if(!$existingRecord) {
+            return response()->json(['message' => 'Alarm not found'], 404);
+        }
+
+        $existingRecord->update([
+            'analyze_alarm_activated_date' => null,
+            'is_analyze_alarm_activated' => false,
+            'is_active' => false,
+        ]);
+
+        return response()->json(['message' => 'Alarm seen'], 200);
+    }
+
+    public function ActivateAlarm(ActivateAlarmRequest $request) {
+        $user = auth()->user();
+
+        $cryptoCurrency = CryptoCurrency::find($request->crypto_currency_id);
+
+        $existingRecord = CryptoCurrencyUser::where('user_id', $user->id)->where('crypto_currency_id', $cryptoCurrency->id)->first();
+
+        if(!$existingRecord) {
+            return response()->json(['message' => 'Alarm not found'], 404);
+        }
+
+        $existingRecord->update([
+            'analyze_alarm_activated_date' => null,
+            'is_analyze_alarm_activated' => false,
+            'is_active' => true,
+        ]);
+
+        return response()->json(['message' => 'Alarm activated'], 200);
     }
 
     /**
@@ -101,5 +158,7 @@ class UserController extends Controller
 
         return response()->json(['message' => 'CryptoCurrency detached successfully'], 200);
     }
+
+
 
 }
